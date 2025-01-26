47d1c4226e71829ab2d0e4a612ca164792c9c5a7
ilia Nikiforov <nikif002@umn.edu>
2025-01-26 16:34:37 -0600

    revert unnecessary change

--- kim_query/__init__.py
+++ kim_query/__init__.py
@@ -1,4 +1,5 @@
 from .kim_query import *
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
